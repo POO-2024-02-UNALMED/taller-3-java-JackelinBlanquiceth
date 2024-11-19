1009eafd52d0cb8ee24204248f0710d00e61f8ed
JackelinBlanquiceth <glozanob@unal.edu.co>
2024-11-18 22:55:50 -0500

    Update TV.java

--- src/main/java/taller3/televisores/TV.java
+++ src/main/java/taller3/televisores/TV.java
@@ -15,62 +15,46 @@ public class TV {
 		this.canal = 1;
 		this.volumen = 1;
 		this.precio = 500;
-		
 		TV.numTV++;
 	}
-	
 	public static int getNumTV() {
 		return TV.numTV;
 	}
-	
 	public static void setNumTV(int numTV) {
 		TV.numTV = numTV;
 	}
-	
 	public void turnOn() {
 		this.estado = true;
 	}
-	
 	public void turnOff() {
 		this.estado = false;
 	}
-	
 	public void canalUp() {
-		this.setCanal(this.canal + 1);
+		  this.setCanal(this.canal++);
 	}
-	
 	public void canalDown() {
-		this.setCanal(this.canal - 1);
+		  this.setCanal(this.canal--);
 	}
-	
 	public void volumenUp() {
-		this.setVolumen(this.volumen + 1);
+		  this.setVolumen( this.volumen++);
 	}
-	
 	public void volumenDown() {
-		this.setVolumen(this.volumen - 1);
+		   this.setVolumen( this.volumen--);
 	}
-	
 	public Marca getMarca() {
 		return this.marca;
 	}
-	
 	public int getCanal() {
 		return this.canal;
 	}
-	
 	public int getPrecio() {
 		return this.precio;
 	}
-	
 	public int getVolumen() {
 		return this.volumen;
-	}
-	
-	public Control getControl() {
+	}	public Control getControl() {
 		return this.control;
 	}
-	
 	public boolean getEstado() {
 		return this.estado;
 	}
@@ -78,22 +62,22 @@ public class TV {
 	public void setMarca(Marca marca) {
 		this.marca = marca;
 	}
-	
 	public void setCanal(int canal) {
-		if (this.estado && canal >= 1 && canal <= 120)
-			this.canal = canal;
+		if (this.estado)
+			if(canal>=1 && canal<=120)
+		        this.canal = canal;
 	}
-	
+
 	public void setPrecio(int precio) {
 		this.precio = precio;
 	}
-	
 	public void setVolumen(int volumen) {
-		if (this.estado && volumen >= 0 && volumen <= 7)
-			this.volumen = volumen;
+		if (this.estado)
+			if(volumen >= 0 && volumen <= 7)
+		        this.volumen = volumen;
 	}
-	
 	public void setControl(Control control) {
 		this.control = control;
 	}
+	
 }
